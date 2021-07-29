19c2a2c54e0ba726268bc1892a914f6e0a03177a
d7aniel <cohl.daniel@gmail.com>
2021-07-29 12:57:02 -0300

    bug por coma

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("v.29)
+console.log("v.29")
 import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js';
 // import {cargarModelo,cargarModeloConAnimacion} from './CargarModelo.js';
 // import {ObjetoModelo} from './Particula.js';
